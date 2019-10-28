881fb07465acbb9e7a104725e39c080cf966c388
lihuanyu01 <lihuanyu01@baidu.com>
2019-10-28 16:30:11 +0800

    feat:modify addRequireConfig

--- src/addRequireConfig.ts
+++ src/addRequireConfig.ts
@@ -14,13 +14,13 @@ export function addRequireConfig (option: OptionType) {
             const fileName = basename(file.path);
             // 主入口定义的moduleId不需要写到config里面
             if (fileName !== mainJs) {
-                const touchModuleId = file.contents.toString().match(/^define\(["']([0-9a-zA-Z@_\-/]+)["']/);
-                const moduleId = touchModuleId && touchModuleId[1];
-                const relativePath = relative(option.sourceDir, file.path);
-                if (moduleId) {
-                    // {'@molecule/a/xxx': '//m.baidu.com/se/static/molecules/a/b/xx}
-                    pathConfig[moduleId] = option.deloyDir + relativePath.replace(/.js$/, '');
-                }
+                const relativePath = path_1.relative(option.sourceDir, file.path);
+                const touchModuleId = file.contents.toString().match(/define\(["']([0-9a-zA-Z@_\-/]+)["']/g);
+                const bundlePath = option.deloyDir + relativePath.replace(/.js$/, '');
+                touchModuleId.map((item) => {
+                    const moduleId = item.replace(/^define\("/,'').replace(/"$/,'');
+                    pathConfig[moduleId] = bundlePath;
+                })
             } else {
                 mainFile = file;
             }

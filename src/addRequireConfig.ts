b62de27af2f630a2f1ae4d87b5f87017c25f6366
lihuanyu01 <lihuanyu01@baidu.com>
2019-10-29 10:53:33 +0800

    feat:eslint

--- src/addRequireConfig.ts
+++ src/addRequireConfig.ts
@@ -14,13 +14,13 @@ export function addRequireConfig (option: OptionType) {
             const fileName = basename(file.path);
             // 主入口定义的moduleId不需要写到config里面
             if (fileName !== mainJs) {
-                const relativePath = path_1.relative(option.sourceDir, file.path);
+                const relativePath = relative(option.sourceDir, file.path);
                 const touchModuleId = file.contents.toString().match(/define\(["']([0-9a-zA-Z@_\-/]+)["']/g);
                 const bundlePath = option.deloyDir + relativePath.replace(/.js$/, '');
                 touchModuleId.map((item) => {
-                    const moduleId = item.replace(/^define\("/,'').replace(/"$/,'');
+                    const moduleId = item.replace(/^define\("/, '').replace(/"$/, '');
                     pathConfig[moduleId] = bundlePath;
-                })
+                });
             } else {
                 mainFile = file;
             }
